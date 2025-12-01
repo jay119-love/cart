3cbcec12cc1d51e9caa9aebbf84748c3920b4231
donghyun Jung <dhyun060630@gmail.com>
2025-12-01 18:00:10 +0900

    first commit

---
+++ static/data.js
@@ -0,0 +1,25 @@
+// static/data.js
+export const productData = [
+    {
+      id: 1,
+      name: "블랙 자켓",
+      desc: "미니멀 블랙 테일러드 자켓",
+      sizes: ["S", "M", "L"],
+      colors: ["black", "white"]
+    },
+    {
+      id: 2,
+      name: "데님 팬츠",
+      desc: "스트레이트 중청 데님 팬츠",
+      sizes: ["M", "L", "XL"],
+      colors: ["blue", "navy"]
+    },
+    {
+      id: 3,
+      name: "코튼 셔츠",
+      desc: "화이트 옥스포드 셔츠",
+      sizes: ["S", "M", "L", "XL"],
+      colors: ["white", "beige"]
+    }
+  ];
+  
